19244be4d9484956470ee998ac30083013d00044
Leif Van Holland <holland@uni-bonn.de>
2019-04-06 15:03:02 +0200

    add .m for first sheet

---
+++ Sheet1/Sheet1Exercise2.m
@@ -0,0 +1,12 @@
+SAMPLING_RATE = 44100; % samples / second
+FREQ = 1000;
+DURATION = 1; % in seconds
+
+n = 0:1/SAMPLING_RATE:1;
+x_sin = sin(FREQ*2*pi*n);
+x_cos = cos(FREQ*2*pi*n);
+
+subplot(1,2,1);
+spectrogram(x_sin, 'yaxis')
+subplot(1,2,2);
+spectrogram(x_cos, 'yaxis')
